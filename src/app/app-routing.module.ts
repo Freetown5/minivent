9281a504e8292d89b1830aa387dc9770f0236816
Isatu Conteh <iconteh@gmail.com>
2023-01-29 16:01:43 -0500

    routed organizer page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './landing/landing.component';
+import { OrganizerComponent } from './organizer/organizer.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'landing', pathMatch: 'full'},
-  { path: 'landing', component: LandingComponent}
+  { path: 'landing', component: LandingComponent},
+  { path: 'organizer', component: OrganizerComponent}
 ];
 
 @NgModule({
