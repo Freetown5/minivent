39781a9c67d2568ce5632b01c4d24ec0883c5228
Isatu Conteh <iconteh@gmail.com>
2023-01-29 15:58:27 -0500

    created comp for organizer page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
+import { OrganizerComponent } from './organizer/organizer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingComponent
+    LandingComponent,
+    OrganizerComponent
   ],
   imports: [
     BrowserModule,
