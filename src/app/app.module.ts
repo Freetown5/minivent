f100e475afbc91e1cdaa6ebdae8bf7286aa0724a
Isatu Conteh <iconteh@gmail.com>
2023-02-10 16:04:07 -0500

    created events form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { LandingComponent } from './pages/landing/landing.component';
 import { OrganizerComponent } from './pages/organizer/organizer.component';
 import { EventCardComponent } from './sections/event-card/event-card.component';
+import { EventsFormComponent } from './sections/events-form/events-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent,
     OrganizerComponent,
-    EventCardComponent
+    EventCardComponent,
+    EventsFormComponent
   ],
   imports: [
     BrowserModule,
