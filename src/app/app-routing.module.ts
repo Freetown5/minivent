d01c6b6a115925c38129fb423d76996e0964d510
Isatu Conteh <iconteh@gmail.com>
2023-02-15 09:19:59 -0500

    routed login page and added to landing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './pages/landing/landing.component';
 import { OrganizerComponent } from './pages/organizer/organizer.component';
+import { LoginComponent } from './pages/login/login.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'landing', pathMatch: 'full'},
   { path: 'landing', component: LandingComponent},
-  { path: 'organizer', component: OrganizerComponent}
+  { path: 'organizer', component: OrganizerComponent},
+  { path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
