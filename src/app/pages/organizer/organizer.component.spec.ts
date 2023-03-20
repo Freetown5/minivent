89624892fef0285b3c2b583d1b84a64261f5c37b
Isatu Conteh <iconteh@gmail.com>
2023-03-20 11:17:00 -0400

    added tests for all new organizer methods

--- src/app/pages/organizer/organizer.component.spec.ts
+++ src/app/pages/organizer/organizer.component.spec.ts
@@ -24,4 +24,40 @@ describe('OrganizerComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should change the toggle value to true', () => {
+    component.showEventForm();
+    fixture.detectChanges();
+    expect(component.eventToggle).toBe(true);
+  });
+
+  it('should change the toggle value to false when true is passed', () => {
+    let testFormSubmitted = true;
+    component.hideOnSubmit(testFormSubmitted);
+    fixture.detectChanges();
+    expect(component.eventToggle).toBe(false);
+  });
+
+  it('should not change the toggle value when false is passed', () => {
+    let testFormSubmitted = false;
+    component.hideOnSubmit(testFormSubmitted);
+    fixture.detectChanges();
+    expect(component.eventToggle).toBe(false);
+  });
+
+  it('should add data to form data object used in the template', () => {
+    let testFormData = {
+      title: 'Fake Event',
+      date: '11/12/2033',
+      time: '2:00pm',
+      region: 'H Street',
+      location: '2343 7St NW',
+      attendeeMax: '15',
+      eventDescription: 'Some stuff would be happening here I guess',
+    }
+
+    component.viewNewData(testFormData);
+    fixture.detectChanges();
+    expect(component.formEventData).toContain(testFormData);
+  })
 });
