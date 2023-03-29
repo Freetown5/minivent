4e9d7d5d8d8ed95deff0b7a076d10cf2a8a699cc
Isatu Conteh <iconteh@gmail.com>
2023-03-29 10:07:05 -0400

    added attendee form and functions

--- src/app/sections/event-card/event-card.component.ts
+++ src/app/sections/event-card/event-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-event-card',
@@ -14,10 +15,27 @@ export class EventCardComponent {
   @Input() attendees: Array<object>;
   @Input() attendeeMax: string;
 
+  constructor(private formBuilder: FormBuilder){} 
+
   attendeesSum: number;
+  formOpen: boolean = false;
+
+  attendeeForm = this.formBuilder.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required]
+  }); 
 
   get attendeeTotal(){
     this.attendeesSum = this.attendees.length;
     return this.attendeesSum;
+  }
+
+  toggleForm(){
+    this.formOpen === false?this.formOpen = true:this.formOpen = false;
+  }
+
+  submitForm(){
+    console.log(this.attendeeForm.value);
+    this.formOpen = false;
   }
 }
